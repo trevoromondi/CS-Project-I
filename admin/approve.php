fce9f8e323b2a65d9ecd170ed2423ea4dc7a0c5f
trevoromondi <otienomondi23@gmail.com>
2021-08-29 13:11:09 +0300

    Message Approval Fix

--- admin/approve.php
+++ admin/approve.php
@@ -22,7 +22,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../style.css">
 
-    <title>Edit</title>
+    <title>Approve Messsage</title>
 </head>
 
 <body>
@@ -41,14 +41,14 @@
                     <div class="form-row">
                         <div class="col-lg-7">
                             <label class="form-label">Alert ID</label>
-                            <input type="number" name="alertID" value="" id="alertID" value="<?php echo $data['alertID']?>" class="form-control my-3 p-4">
+                            <input type="number" name="alertID" value= "<?= $data['alertID'] ?>" id="alertID" class="form-control my-3 p-4">
                         </div>
                     </div>
 
                     <div class="form-row">
                         <div class="col-lg-7">
                         <label class="form-label">Alert</label>
-                         <textarea name="alertDescription" class="form-control" value="<?php echo $data['alertDescription']?>" rows="3"></textarea>
+                         <textarea name="alertDescription" class="form-control" rows="3"><?= $data['alertDescription']?></textarea>
                     </div>
                     </div>
 
@@ -65,7 +65,7 @@
 
                     <div class="form-row">
                         <div class="col-lg-7">
-                            <input type="text" name="officer_id" value="" id="officer_id" value="<?php echo $data['officer_id']?>" class="form-control my-3 p-4">
+                            <input type="text" name="officer_id" id="officer_id" value="<?= $data['officer_id']?>" class="form-control my-3 p-4">
                         </div>
                     </div>
     
