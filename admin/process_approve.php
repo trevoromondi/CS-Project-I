04a342ce491d133934dfe77094b93b50e2a6310e
NaomiMunyiri <naomi.munyiri11@gmail.com>
2021-10-27 22:49:39 +0300

    latest

--- admin/process_approve.php
+++ admin/process_approve.php
@@ -1,7 +1,6 @@
 <?php
 require_once('db_connect.php');
 require '../vendor/autoload.php';
-//use autoload;
 
 if(isset($_POST['submit']))
 {
@@ -11,14 +10,10 @@ if(isset($_POST['submit']))
     $status=$_POST["status"];
 
     $sql = "UPDATE `alert` SET `alertID`='$alertID',`alertDescription`='$alertDescription',`status`= '$status',`officer_id`='$officer_id' WHERE alertID='$alertID'";
-    
-    if($status=='Approved')
-    {
       
       $phoneNumbers=array();
       if(mysqli_query($conn, $sql))
       {
-          //$conn = new mysqli('localhost','root','','cs_project');
           $result = $conn->query("SELECT phone_number FROM citizens");
           if ($result->num_rows > 0) 
           {
@@ -31,20 +26,14 @@ if(isset($_POST['submit']))
           {
               echo "0 results";
           }
-          //print_r($phoneNumbers);
-          $client = new MessageBird\Client('JcxdYSxeEs3kuG8bmHJjVaP05');
-       
-         //echo "Success";
-         //print_r($client);
+          //Messaging API
+          $client = new MessageBird\Client('FZudI4SK9uLS61g0BiGy9JiuJ');
 
-        //if($status='Approved')
-        //{
             $message = new MessageBird\Objects\Message;
-            $message->originator = '+254706253490';
+            $message->originator = '+2547********';
             $message->recipients = $phoneNumbers;
             $message->body = $alertDescription;
             $response = $client->messages->create($message);
-            //print_r($response);
         
             echo '<script>alert("Message Approved and Sent")</script>';
             echo '<script>window.location="messagesent.php"</script>';
@@ -52,16 +41,9 @@ if(isset($_POST['submit']))
       }else
          {
             echo '<script>alert("Try Again")</script>';
-            //echo "something went wrong ";
+
          }
 
-    }else if($status=='Denied')
-    {
-      if(mysqli_query($conn, $sql))
-      {
-      echo '<script>alert("The message will not be sent out to the public")</script>';
-      echo '<script>window.location="alerts.php"</script>';
-      }
-    }
+    
 }
 ?>
